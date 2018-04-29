456dfacb37a0f50c3a4fd5a2c2497a580a5403ad
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-29 15:30:51 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,22 +1,46 @@
 require 'pry'
 def get_first_name_of_season_winner(data, season)
   # code here
-  data[season]
-  binding.pry
+  data[season].select do |item|
+      item["status"] == "Winner"
+  end.first["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, contestants|
+      contestants.each do |contestant|
+          return contestant["name"] if contestant["occupation"] == occupation
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  counter = 0
+  data.each do |season, contestants|
+      contestants.each do |contestant|
+          counter += 1 if contestant["hometown"] == hometown
+      end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
   # code here
+  data.each do |season, contestants|
+      contestants.each do |contestant|
+          return contestant["occupation"] if contestant["hometown"] == hometown
+      end
+  end
 end
 
 def get_average_age_for_season(data, season)
   # code here
+  arr = data[season].collect do |item|
+      item["age"]
+  end
+  i = arr.size
+  arr = arr.join("+")
+  eval(arr).fdiv(i).round
 end
